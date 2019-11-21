9b05fa1e8182e4b0bf7a8e5d4f5740412eda6af6
alexsmith716 <alexsmith716@outlook.com>
2019-11-21 12:38:48 -0500

    refactor: Modified babelrc it is now a exported commonjs module.

---
+++ .babelrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      'useBuiltIns': 'usage',
+      'corejs': {
+        'version': 3,
+        'proposals': true
+      },
+      'debug': false
+    }],
+    '@babel/preset-react',
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['@babel/plugin-transform-runtime',{'corejs': {'version': 3,'proposals': true}}],
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    '@babel/plugin-transform-async-to-generator',
+    '@babel/plugin-syntax-dynamic-import',
+    'react-hot-loader/babel',
+    'universal-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-export-default-from'
+  ]
+};
